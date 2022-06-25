ae35faba6cb0e0bd45e4591358bfad848a60c898
kevinchin37 <kevin.chin34@gmail.com>
2022-06-25 16:12:35 -0400

    Remove comments

--- src/components/UI/Header/Banner.js
+++ src/components/UI/Header/Banner.js
@@ -1,11 +1,8 @@
 import classes from './Banner.module.css';
-import bannerImg from '../../../assets/images/bg-header-desktop.svg';
 
 const Banner = () => {
     return (
-        <header className={classes.banner}>
-            {/* <img className={classes['banner-image']} src={bannerImg} /> */}
-        </header>
+        <header className={classes.banner}></header>
     )
 }
 
