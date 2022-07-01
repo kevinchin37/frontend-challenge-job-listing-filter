2276372fd4ac5fe3c6aa673fda9d2fcd4339e0f5
kevinchin37 <kevin.chin34@gmail.com>
2022-07-01 14:59:55 -0400

    Add debounce and a loading state with a spinner for filter updates

--- src/components/Job/JobList.js
+++ src/components/Job/JobList.js
@@ -1,34 +1,56 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import DUMMY_DATA from '../data.json';
 import JobListItem from "./JobListItem";
 import FilterContext from "../../store/filter-context";
 import ActiveFilterList from "../Filter/ActiveFilterList";
+import Spinner from "../UI/Layout/Spinner";
 
 const JobList = () => {
     const filterContext = useContext(FilterContext);
+    const [jobs, setJobs] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    let jobPostings = [];
+    let hasActiveFilters = filterContext.filters.length > 0 ? true : false;
 
-    let hasActiveFilters = false;
-    if (filterContext.filters.length > 0) {
-        hasActiveFilters = true;
-    }
+    useEffect(() => {
+        let filterSetTimer;
 
-    if (hasActiveFilters) {
-        jobPostings = DUMMY_DATA.filter(job => {
-            return filterContext.filters.every(filter => {
-                return job.languages.includes(filter);
-            });
-        })
-    } else {
-        jobPostings = DUMMY_DATA;
-    }
+        if (hasActiveFilters) {
+            setIsLoading(true);
+
+            /*
+                Job list shouldn't re-render immediately on the event of a filter being clicked.
+                This would especially be helpful if data was being fetched instead as it would help lessen api calls.
+                Added a debounce of 1.5 secs to give more time to apply filters.
+            */
+            filterSetTimer = setTimeout(() => {
+                const filteredJobs = DUMMY_DATA.filter(job => {
+                    return filterContext.filters.every(filter => {
+                        return job.languages.includes(filter);
+                    });
+                });
+
+                setJobs(filteredJobs);
+                setIsLoading(false);
+            }, 1500);
+
+        } else {
+            setJobs(DUMMY_DATA);
+            setIsLoading(false);
+        }
+
+        return () => {
+            clearTimeout(filterSetTimer);
+        }
+    }, [filterContext.filters]);
 
     return (
         <React.Fragment>
             {hasActiveFilters && <ActiveFilterList />}
 
-            {jobPostings.map(job => (
+            {isLoading && <Spinner>Applying Filters</Spinner>}
+
+            {jobs.map(job => (
                 <JobListItem key={job.id} item={job} />
             ))}
         </React.Fragment>
